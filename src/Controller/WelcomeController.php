6693425a89e343923d05f26c29f83cba839eb60a
shin yuh wang <7535835-vikki2021@users.noreply.gitlab.com>
2021-03-04 18:15:03 +0100

    first commit

---
+++ src/Controller/WelcomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class WelcomeController extends AbstractController
+{
+    /** @Route("/welcome") **/
+    public function homepage(): Response
+    {
+        return $this->render('welcome.html.twig', ['day' => date('l')]);
+    }
+}
